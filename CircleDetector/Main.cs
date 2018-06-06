47b4867540806f380c01d77f9ddcb921b89ee954
Muhammad Vakili <m.vakili95@gmail.com>
2018-06-07 00:11:49 +0430

    Created README.md

---
+++ CircleDetector/Main.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+
+namespace Circle
+{
+    public partial class Main : Form
+    {
+        Capture webcamCapture;
+        Image<Bgr, Byte> mainImage;
+        Image<Gray, Byte> processedImage;
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                webcamCapture = new Capture();
+            }
+            catch (Exception ex)
+            {
+                txtLog.Text += ex.Message;
+            }
+
+            Application.Idle += Application_Idle;
+        }
+
+        private void Application_Idle(object sender, EventArgs e)
+        {
+            mainImage = webcamCapture.QueryFrame().ToImage<Bgr, Byte>();
+            if (mainImage == null) return;
+            processedImage = mainImage.InRange(new Bgr(120, 0, 0), new Bgr(256, 70, 70));
+            processedImage = processedImage.SmoothGaussian(9);
+            CircleF[] circles = processedImage.HoughCircles(new Gray(100), new Gray(50), 2, processedImage.Height / 4, 10, 400).SelectMany(u => u.Select(s => s)).ToArray();
+            foreach (var c in circles)
+            {
+                if (txtLog.Text != "") txtLog.AppendText(Environment.NewLine);
+
+
+                CvInvoke.Circle(mainImage, new Point((int)c.Center.X, (int)c.Center.Y), 3, new MCvScalar(0, 255, 0), -1, Emgu.CV.CvEnum.LineType.AntiAlias, 0);
+                mainImage.Draw(c, new Bgr(Color.Red), 3);
+            }
+            imageBox1.Image = mainImage;
+            imageBox2.Image = processedImage;
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,28 @@
+# Live Circle Detector
+
+
+### Preview
+
+![Preview](preview.jpg)
+
+# How to use
+
+## 1st step:
+```
+> git clone https://github.com/mvakili/ImageProcessing.git
+```
+
+## 2nd step:
+    Open ImageProcessing.sln using Visual Studio
+
+## 3rd step:
+    Restore missing packages using: 
+    Tool > NuGet Package Manager > Manage Nuget Packages for Solution
+
+    If there is any missing package in solution, the Restore button will be shown at the top-right side of screen. By clicking the Restore button, all missing packages will be restore.
+
+## 4th step:
+    Run and enjoy
+
+## License
+[MIT](https://github.com/mvakili/ImageProcessing/blob/master/LICENSE)
